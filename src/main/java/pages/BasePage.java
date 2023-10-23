83003388c00609d5b08569f9dbd91224714c118c
Deiu <selagea.adnrei89@gmail.com>
2023-10-23 23:54:44 +0300

    First commit final projetct

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -3,33 +3,36 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+
 
 import java.nio.file.Paths;
 
 public class BasePage {
 
-
     protected WebDriver driver;
-    private String baseUrl = "https://fasttrackit-test.netlify.app/#/";
+    private String baseURL = "https://ecommerce-playground.lambdatest.io/";
     public BasePage() {
-// Default constructor
+
     }
-    public BasePage(WebDriver driver) {
+    public BasePage(WebDriver driver){
         this.driver = driver;
+
     }
+
     @BeforeClass
+
     public void setUp() {
         String driverPath = Paths.get("src/main", "resources/", "chromedriver.exe").toString();
         System.setProperty("web - driver.chrome.driver", "\"D:\\Curs testare\\chromedriver_win32\\chromedriver.exe\"");
         System.setProperty("web-driver.chrome.driver", driverPath);
         driver = new ChromeDriver();
-        driver.get(baseUrl);
-    }
-    @AfterMethod
+        driver.manage().window().maximize();
+        driver.get(baseURL);
 
-    public void tearDown() {
-        driver.quit();
+    }
+    @AfterClass
+
+    public void tearDown() {driver.quit();
     }
 }
